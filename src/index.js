76767418dd7a7c58392660e96517ce4bb14c8f43
eliasrocha82p <eliasrocha82p@gmail.com>
2024-01-28 21:13:32 -0300

    master

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {HashRouter} from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+React.render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
