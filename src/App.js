9455c2bde92584c69dc75ef04fcc804e6d0a0d8e
eliasrocha82p <eliasrocha82p@gmail.com>
2024-01-29 11:48:54 -0300

    master

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   render() {
       return (
         <div className="App">
-            <h1>teste</h1>
+            <About />
           </div>
           );
   }
