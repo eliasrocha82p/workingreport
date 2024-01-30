6a9ce71a7d04106599fbbc099a8c96d8c6396c49
eliasrocha82p <eliasrocha82p@gmail.com>
2024-01-30 20:39:41 -0300

    master

--- src/App.js
+++ src/App.js
@@ -4,17 +4,12 @@ import { HashRouter, Route, Routes, Link } from "react-router-dom";
 import About from './About';
 import Home from './Home';
 
-class App extends React.Component {
-  render() {
+function App() {
       return (
-        <HashRouter>
-    <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-  </HashRouter>
+        <h1>
+        <About />
+        </h1>
           );
-  }
 }
 
 export default App;
