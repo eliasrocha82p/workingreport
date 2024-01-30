658fe9a282beb05726fa6753290516b96732e910
eliasrocha82p <eliasrocha82p@gmail.com>
2024-01-30 20:44:13 -0300

    master

--- src/About.jsx
+++ src/About.jsx
@@ -4,18 +4,13 @@ const divStyle = {
     color:'white'
 };
 
-class About extends React.Component {
-    
-    render() {
+class About(){
         return (
-            <div style={divStyle}>
                 <h2>About Page</h2>
                 <main>
                     <p>This section contains information about...</p>
                 </main>
-            </div>
         )
-    }
 }
 
 
