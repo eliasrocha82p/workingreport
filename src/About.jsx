a3fac2d48c44c575b5797273f7ddf6e14bbac112
eliasrocha82p <eliasrocha82p@gmail.com>
2024-01-30 20:48:45 -0300

    master

--- src/About.jsx
+++ src/About.jsx
@@ -7,9 +7,7 @@ const divStyle = {
 function About(){
         return (
                 <h2>About Page</h2>
-                <main>
                     <p>This section contains information about...</p>
-                </main>
         )
 }
 
